94944e7b506ed54a7ff85ebdbac17e841f9e436c
TamashiiMon <tamashiimon@gmail.com>
2026-08-08 11:34:02 +0200

    test behavior

--- modules/default/WorldGenFeature/src/main/java/space/qouve/worldgenfeature/models/WorldGenBehaviors.java
+++ modules/default/WorldGenFeature/src/main/java/space/qouve/worldgenfeature/models/WorldGenBehaviors.java
@@ -1,10 +1,12 @@
 package space.qouve.worldgenfeature.models;
 
+import space.qouve.worldgenfeature.behaviors.WaterPlaceBehavior;
+
 public final class WorldGenBehaviors {
 
     private WorldGenBehaviors() {}
 
     public static void registerAll() {
-        // ... All conditions here
+        new WaterPlaceBehavior();
     }
 }
